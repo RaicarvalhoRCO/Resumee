daa24b461fe99146b9f6ad0f5896d7e5ebe6c41b
RaicarvalhoRCO <rai_sp16@outlook.com>
2025-02-27 15:09:30 -0300

    Education + Skills rebuilt

--- interactive-resume/src/app/education/education.component.ts
+++ interactive-resume/src/app/education/education.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education.component.html',
-  styleUrl: './education.component.css'
+  styleUrls: ['./education.component.css']
 })
 export class EducationComponent {
-
+  education = [
+    {
+      title: 'Bachelor of Computer Science',
+      school: 'UNIFESP – Universidade Federal de São Paulo',
+      period: 'Januray 2016 - June 2018',
+    },
+    {
+      title: 'Bachelor Degree in Database',
+      school: 'FAM - Centro Universitário',
+      period: 'January 2024 - Present',
+    }
+  ];
 }
--- interactive-resume/src/app/experience/experience.component.ts
+++ interactive-resume/src/app/experience/experience.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
-
+  experiences = [
+    {
+      title: 'Front-End developer',
+      company: 'Freelance',
+      period: 'May 2024 - Present',
+      description: 'Responsible for designing and implementing newfeatures and maintaining existing code',
+      skills: ['Angular', 'TypeScript', 'HTML5/CSS3', 'JavaScript', 'Node.js', 'Git', 'Figma']
+    },
+    {
+      title: 'Product Owner',
+      company: 'InEvent',
+      period: 'May 2024 - November 2024',
+      description: 'Responsible for translating the voice of customers within the organization and delivering maximum value in product development.',
+      skills: ['PHP', 'Jira', 'Figma', 'Slack']
+    },
+    {
+      title: 'Systems analyst and developer',
+      company: 'KYGA Consultancy',
+      period: 'June 2022 - April 2024',
+      description: ' Conduct detailed testing and precise documentation of Ariba system APIs, ensuring all integrations and functionalities align with corporate standards.',
+      skills: ['JavaScript', 'ABAP', 'ERP-SAP', 'Ariba - Buying', 'Ariba - Sourcing','Ariba - SLP',]
+    },
+    {
+      title: 'IT Support and Computer Network Analyst',
+      company: 'Company Z',
+      period: 'December 2020 - May 2022',
+      description: 'Responsible for providing first-level support to employees (Windows/Office), analyzing and managing computer networks (intranet), and delivering specialized support for TASY and TOTVS systems',
+      skills: ['TASY', 'TOTVS', 'Office', 'Hardware maintenance']
+    }
+  ];
 }
